a5231b859d4acc216592a16d2be5b1532180c638
Everi Osofsky <eosofsky@andrew.cmu.edu>
2017-04-27 21:46:16 -0400

    Bus UI

---
+++ Assets/Scripts/Bus UI/IconManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IconManager : MonoBehaviour {
+
+	public Sprite Icon1;
+	public Sprite Icon2;
+	public Sprite IconBoss;
+
+	void Awake () {
+		if (SourceManager.oldScene.Equals ("Bus Exterior 1") || SourceManager.oldScene.Equals ("Bus Exterior 2")) {
+			Image image1 = GameObject.Find ("Bus UI 1").GetComponent<Image> ();
+			image1.sprite = Icon1;
+		}
+	}
+}
---
+++ Assets/Scripts/Bus UI/Travel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Travel : MonoBehaviour {
+
+	public void GoToTerminalA () {
+		SceneManager.LoadScene ("Bus Station");
+	}
+
+	public void GoToTerminalB () {
+		SceneManager.LoadScene ("Bus Station 2");
+	}
+
+	public void GoToBossRoom () {
+		SceneManager.LoadScene ("Boss Room");
+	}
+
+}
